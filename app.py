ba0ec50241f485758e53c1c357e3454022325a90
Tarun <84436181+code-simp@users.noreply.github.com>
2021-12-26 17:27:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,jsonify
-from flask_restful import Resource, Api
+from flask.wrappers import Request
+from flask_restful import Resource, Api, reqparse
 import psycopg2 
 
 # Connecting the DB
@@ -9,35 +10,61 @@ con = psycopg2.connect(
     user = 'postgres',
     password = '1234')
 
+con.autocommit = True
+
 # cursor and queries
 cur = con.cursor()
-
-# cur.execute('select * from card')
-# try1 = cur.fetchall()
-# for i in try1:
-#     print(i)
 
 app = Flask(__name__)
 api = Api(app)
 
+# function to add new card to DB
 
-
-
-class HelloWorld(Resource):
+class AddCard(Resource):
     def get(self):
-        return { 'hello' : 'world'}
+        cur.callproc('insert_to_card')
+        return True
 
-class fetch_card(Resource):
+# function to recharge  MTROCRD26122021-0005
+
+class Recharge(Resource):
+    def get(self,cardNo,amount):
+        if amount > 50:
+            cur.callproc('recharge',(amount,cardNo[:16],cardNo[16:]))
+            return True
+        else:
+            return False 
+
+# function to retrieve all tuples of the card
+
+class History(Resource):
+    def get(self,cardNoTemp):
+        cardNo = cardNoTemp[:16]
+        for i in range(16,len(cardNoTemp)):
+            if cardNoTemp[i] != '0':
+                break
+            else:
+                continue
+        print(cardNoTemp)
+        cardNo += cardNoTemp[i:]
+        print(cardNo)
+        cur.execute("select * from transactions where concat(card_id_1,card_id_2) = '%s'" %(cardNo))
+        history = cur.fetchall()
+        return jsonify(history)
+
+# function to retrieve all tuples of transaction table
+
+class History_all(Resource):
     def get(self):
-        cur.execute('select * from card')
-        try1 = cur.fetchall()
-        for i in try1:
-            print(i)
-        return jsonify(try1)
+        cur.execute('select * from transactions')
+        historyAll = cur.fetchall()
+        return jsonify(historyAll)
         
 
-api.add_resource(HelloWorld,'/')
-api.add_resource(fetch_card,'/fc')
+api.add_resource(AddCard,'/add_card')
+api.add_resource(History, '/history/<string:cardNoTemp>')
+api.add_resource(History_all, '/history_all')
+api.add_resource(Recharge,'/recharge_card/<string:cardNo>/<int:amount>')
 
 
 if __name__ == '__main__':
