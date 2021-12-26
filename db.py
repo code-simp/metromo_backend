62c5571d7b09ffabe3059997ac55d0397fdf90d9
Tarun <84436181+code-simp@users.noreply.github.com>
2021-12-26 10:42:38 +0530

    Update db.py

--- db.py
+++ db.py
@@ -19,7 +19,10 @@ cur = con.cursor()
 cur.callproc('insert_to_card')
 
 # Python function to recharge
-cur.callproc('recharge',(200,'MTROCRD26122021-',5))
+amt = 90
+cardNo = 'MTROCRD26122021-0002'
+
+cur.callproc('recharge',(amt,cardNo[:16],cardNo[16:]))
 
 # Python function to check all transactions of a card number
 cardNoTemp = 'MTROCRD26122021-0002'
@@ -34,9 +37,18 @@ cardNo += cardNoTemp[i:]
 
 cur.execute("select * from transactions where concat(card_id_1,card_id_2) = '%s'" %(cardNo))
 
-rows = cur.fetchall() #stored in form of tuples
-for i in rows:
+trans1 = cur.fetchall() #stored in form of tuples
+for i in trans1:
     print(list(i))
+
+cur.close()
+
+# Python function to retrieve all the items in transactions table
+
+cur.execute('select * from transactions')
+trans2 = cur.fetchall()
+for i in trans2:
+    print(i)
 
 # closing the cursor
 cur.close()
